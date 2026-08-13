58edfcf1650c345100e969a605280f8b94b8b4c8
ASHI JAIN <156345119+Ashijain30@users.noreply.github.com>
2026-08-13 11:36:58 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int>arr;
+        int n = nums.size();
+        for(int i = 0; i <n;i++){
+            for(int j = i+1;j<n;j++){
+                if(nums[i]+ nums[j]== target){
+                    arr.push_back(i);
+                    arr.push_back(j);
+
+                    
+                }
+
+            }
+        }
+     return arr;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    
